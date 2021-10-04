f890d5168a817768a03e278ca0388ba52e99f904
tanveerm176 <tanveerm176@gmail.com>
2021-10-03 22:35:25 -0400

    added myEvery(), myReduce(), and myIncludes()

--- Assignment2.js
+++ Assignment2.js
@@ -63,3 +63,87 @@ function mySome(arr, callback) {
     }
     return false;
 }
+
+// const array = [1, 2, 3, 5];
+// // checks whether an element is even
+// function even(element) {
+//     if (element % 2 === 0) {
+//         console.log("true" + element);
+//         return true;
+//     }
+//     else {
+//         console.log("else" + element);
+//         return false;
+//     }
+// }
+// console.log(mySome(array, even));
+
+function myEvery(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!callback(arr[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// function test(element)
+// {
+//     if(element < 40)
+//     {
+//         return true;
+//     }
+
+//     else
+//     {
+//         return false;
+//     }
+// }
+// const array1 = [1, 30, 51, 29, 10, 13];
+// console.log(myEvery(array1,test));
+
+function myReduce(arr, callback) 
+{   
+    prevVal = null;
+    for (let i = 1; i < arr.length; i++) 
+    {
+        if(!prevVal)
+        {
+            prevVal = arr[i-1];
+        }
+        prevVal = callback(prevVal, arr[i]);
+        
+    }
+    return prevVal;
+}
+
+// function add(x,y)
+// {
+//     return x+y;
+// }
+// const array1 = [1, 2, 3, 4];
+// console.log(myReduce(array1, add))
+
+
+function myIncludes(arr, searchElement, fromIndex = 0) {
+
+    if(fromIndex<0 && (Math.abs(fromIndex))>arr.length)
+    {
+        fromIndex = 0;
+    }
+    else
+    {
+        fromIndex = arr.length+fromIndex;
+    }
+
+    for (let i = fromIndex; i < arr.length; i++) {
+        if (arr[i] === searchElement) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// const array1 = [1, 2, 3,4];
+// console.log(myIncludes(array1,4,-1))
+
